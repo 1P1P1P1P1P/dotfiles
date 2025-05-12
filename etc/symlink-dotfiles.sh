17ed2ed2b7b1c0cfa15328c5efc0ed2b389f8ceb
VV1P1n <wuyiiiiip@gmail.com>
2025-05-12 10:49:33 +0800

    init: create dotfiles

---
+++ etc/symlink-dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+[ "${SHELL##/*/}" != "zsh" ] && echo 'You might need to change default shell to zsh: `chsh -s /bin/zsh`'
+dotfiles="$HOME/Develop/personal/dotfiles"
+
+echo ""
+if [ -d "$dotfiles" ]; then
+  echo "Symlinking dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+link() {
+  from="$1"
+  to="$2"
+  echo "Linking '$from' to '$to'"
+  ln -sf "$from" "$to"
+}
+
+link "$dotfiles/home/zshrc" "$HOME/.zshrc"
+link "$dotfiles/home/p10k.zsh" "$HOME/.p10k.zsh"
+link "$dotfiles/home/oh-my-zsh" "$HOME/.oh-my-zsh"
+link "$dotfiles/home/config/nvim" "$HOME/.config/nvim"
