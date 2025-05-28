d781a24c42bfd3245b660d95c16dfdc73ab527e3
VV1P1n <wuyiiiiip@gmail.com>
2025-05-28 17:31:07 +0800

    feat: add polybar config

---
+++ dot_config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# terminate already running bar instances
+killall -q polybar
+
+# wait untill the processes have been shutdown
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; down
+
+exec polybar --reload main
