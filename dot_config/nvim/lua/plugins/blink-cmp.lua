82404d3bfba4c977e320ca809235d7615c9e71bf
VV1P1n <wuyiiiiip@gmail.com>
2025-06-02 11:13:32 +0800

    fix: fix some keybinding bugs of nvim

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -1,18 +1,34 @@
 return {
-    "saghen/blink.cmp",
-    opts = function(_, opts)
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
         -- don't show the menu automatically
-        opts.completion.menu.auto_show = false
+        auto_show = false,
+      },
+      documentation = {
         -- don't show documentation automatically
-        opts.completion.documentation.auto_show = false
+        auto_show = false,
+      },
+      ghost_text = {
         -- don't display the preview text
         -- enabled = vim.g.ai_cmp
-        opts.completion.ghost_text.enabled = false
-        opts.keymap.preset = "enter"
-        opts.keymap["<Tab>"] = {"select_next", "fallback"}
-        opts.keymap["<S-Tab>"] = {"select_prev", "fallback"}
-        opts.keymap["<C-d>"] = {"show_documentation", "hide_documentation", "fallback"}
-        opts.keymap["<C-m>"] = { "show", "hide", "fallback"}
-        opts.keymap["<C-y>"] = { "select_and_accept" }
-    end,
+        enabled = false,
+      },
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = true,
+        },
+      },
+    },
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<C-d>"] = { "show_documentation", "hide_documentation", "fallback" },
+      ["<C-space>"] = { "show", "hide", "fallback" },
+      ["<C-y>"] = { "select_and_accept" },
+    },
+  },
 }
